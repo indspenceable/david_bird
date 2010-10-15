058eef8a2e7e17124ff93b5447a915c71b40298f
Danny Spencer <dspencer@oberlin.edu>
2010-10-15 10:52:14 -0400

    Made simpler url routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ DavidBird::Application.routes.draw do
   get "site/index"
 
   get "site/bio"
+  match 'bio' => 'site#bio'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +53,7 @@ DavidBird::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "site#index"
 
   # See how all your routes lay out with "rake routes"
 
