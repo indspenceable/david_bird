b0f44b8c9416cc3c78c31a6b760e2bc49b3881b4
Danny Spencer <dspencer@oberlin.edu>
2010-10-24 16:04:57 -0400

    changed layout to the bicolumn layout we discussed. fixing up alignment, but the left column stays centered, for now.

---
+++ public/javascripts/align.js
@@ -0,0 +1,54 @@
+function getWindowHeight() {
+  var windowHeight = 0;
+  if (typeof(window.innerHeight) == 'number') {
+    windowHeight = window.innerHeight;
+  }
+  else {
+    if (document.documentElement && document.documentElement.clientHeight) {
+      windowHeight = document.documentElement.clientHeight;
+    }
+    else {
+      if (document.body && document.body.clientHeight) {
+        windowHeight = document.body.clientHeight;
+      }
+    }
+  }
+  return windowHeight;
+}
+function f_scrollTop() {
+  var ScrollTop = document.body.scrollTop;
+
+  if (ScrollTop == 0)
+  {
+    if (window.pageYOffset)
+      ScrollTop = window.pageYOffset;
+    else
+      ScrollTop = (document.body.parentElement) ? document.body.parentElement.scrollTop : 0;
+  }
+  return ScrollTop;
+}
+function setContent() {
+  if (document.getElementById) {
+    var windowHeight = getWindowHeight();
+    if (windowHeight > 0) {
+      var contentElement = document.getElementById('header');
+      var contentHeight = contentElement.offsetHeight;
+      if (windowHeight - contentHeight > 0) {
+        contentElement.style.position = 'relative';
+        contentElement.style.top = f_scrollTop() + ((windowHeight / 2) - (contentHeight / 2)) + 'px';
+      }
+      else {
+        contentElement.style.position = 'static';
+      }
+    }
+  }
+}
+window.onload = function() {
+  setContent();
+}
+window.onresize = function() {
+  setContent();
+}
+window.onscroll = function() {
+  setContent();
+}
